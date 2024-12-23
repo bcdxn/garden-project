e10fc6f06af9a7a4ece6321a7b6e9b1708ce9ad4
Benjamin Dixon <5067649+bcdxn@users.noreply.github.com>
2024-12-23 16:01:35 -0500

    fix: db migration down order

--- db/migrations/000002_create_rbac_tables.down.sql
+++ db/migrations/000002_create_rbac_tables.down.sql
@@ -1,4 +1,4 @@
-DROP TABLE IF EXISTS rbac_permission;
-DROP TABLE IF EXISTS rbac_resource;
 DROP TABLE IF EXISTS rbac_roles_permissions;
 DROP TABLE IF EXISTS rbac_app_users_roles;
+DROP TABLE IF EXISTS rbac_permission;
+DROP TABLE IF EXISTS rbac_resource;
